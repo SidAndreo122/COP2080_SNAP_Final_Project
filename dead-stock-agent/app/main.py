0f27179ab122cc51abc01fba565093e304acb176
Nathan Silvey <nsilvey2006@gmail.com>
2026-04-21 16:51:26 +0000

    init - initial commit and begin work on dashboard

--- dead-stock-agent/app/main.py
+++ dead-stock-agent/app/main.py
@@ -1,1 +1,16 @@
 # streamlit 
+import streamlit as st
+import matplotlib
+
+path = "dead-stock-agent/data"
+doc_path = f"{path}/docs"
+
+
+def main():
+    st.title("Dead Stock Agent")
+    
+    
+    # Example of using matplotlib to display a simple plot
+    st.subheader("Inventory Analysis")
+   
+    st.pyplot(fig)
